ca5490cd4a17de65ef5a83821dbecc20656ea9df
Юля Кобзева <selemina@mail.ru>
2022-09-27 07:05:43 +0300

    ДЗ 1, задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Flower rose = new Flower("Роза обыкновенная", "", "Голандия", 35.59,0);
+        Flower chrizantema = new Flower("Хризантема", "", "", 15.0, 5);
+        Flower pion = new Flower("Пион", "", "Англия", 69.9, 1);
+        Flower gipsophila = new Flower("Гипсофила", "", "Турция", 19.5, 10);
+
+        System.out.println(rose);
+        System.out.println(chrizantema);
+        System.out.println(pion);
+        System.out.println(gipsophila);
     }
 }
